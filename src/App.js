e1131b3bd4ede081945d945d37e563a999300363
rehadhasan <rehadhasan664@gmail.com>
2023-06-09 22:36:24 +0600

    update App components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import About from "./components/About/About";
+import Banner from "./components/Banner/Banner";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import Services from "./components/Services/Services";
+import Work from "./components/Work/Work";
 
 function App() {
-  return <div>app</div>;
+  return (
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+      <Header></Header>
+      <Banner></Banner>
+      <Nav></Nav>
+      <About></About>
+      <Services></Services>
+      <Work></Work>
+      <Contact></Contact>
+      <div className="h-[4000px]"></div>
+    </div>
+  );
 }
 
 export default App;
