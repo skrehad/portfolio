7798d2d486ceaba63ebc0b1b9a06534a9e661ffd
rehadhasan <rehadhasan664@gmail.com>
2023-06-10 11:37:29 +0600

    update nav.js file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,17 +7,35 @@ const Nav = () => {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto">
         <div className="w-full items-center bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50">
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="home"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiHomeAlt></BiHomeAlt>
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="about"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BiUser></BiUser>
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="services"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsClipboardData></BsClipboardData>
           </Link>
-          <Link className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center">
+          <Link
+            to="work"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
             <BsBriefcase></BsBriefcase>
+          </Link>
+          <Link
+            to="contact"
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
+          >
+            <BsChatSquare></BsChatSquare>
           </Link>
         </div>
       </div>
