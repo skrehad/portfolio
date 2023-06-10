77a2347961f983024522b628b4439ff8d608efe0
rehadhasan <rehadhasan664@gmail.com>
2023-06-10 12:50:13 +0600

    complete header file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Logo from "../../assets/LogoName.png";
+import { Link } from "react-scroll";
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className=" flex justify-between items-center">
+          <Link>
+            <img src={Logo} alt="" />
+          </Link>
+          <button className="btn btn-sm">Work with me </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
