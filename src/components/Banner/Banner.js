2e2033c2da48397132d5da9af2d067ad03109068
rehadhasan <rehadhasan664@gmail.com>
2023-06-12 23:47:16 +0600

    updated Banner section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,57 @@
 import React from "react";
 
+import Image from "../../assets/avatar.svg";
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variant";
+import { Link } from "react-scroll";
+
 const Banner = () => {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+              Saiful Islam <span>Rehad</span>
+            </h1>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={["Developer", 2000, "Designer", 2000]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="mb-8 max-w-lg mx-auto lg:mx-0 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum,
+              fugiat asperiores repudiandae possimus molestiae iusto.
+            </p>
+            <div className="flex max-w-max items-center mb-12 mx-auto lg:mx-0">
+              <button className="btn btn-lg">Contact Me</button>
+              <Link className="text-gradient btn-link">My Portfolio</Link>
+            </div>
+            <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <Link>
+                <FaYoutube />
+              </Link>
+              <Link>
+                <FaGithub />
+              </Link>
+              <Link>
+                <FaDribbble />
+              </Link>
+            </div>
+          </div>
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
